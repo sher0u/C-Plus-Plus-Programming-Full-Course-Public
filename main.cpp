232e01e0540248bdba62a9d6a8a6e551ddfc591a
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-10 23:56:20 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout <<"Welcome To my cours" << std::endl; // << std::endl; for making new line 
+    std::cout << "From Kader"  << '\n'; // << '\n' we can use it as a new line
+    return 0;
+}
