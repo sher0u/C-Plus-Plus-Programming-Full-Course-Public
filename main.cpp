9c8220d7cb79db9a1fb4c58f04c56bbd1cdb0aef
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-21 01:36:08 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iomanip>
+#include <iostream>
+
+//Arithmetic operation [+ - / *}
+
+int main() {
+    int student =20;
+    student = student+2;
+
+    std::cout << student << std::endl;
+    student ++;
+    std::cout << student << std::endl;
+    student = student-2;
+    std::cout << student << std::endl;
+    student = student*2;
+    std::cout << student << std::endl;
+    student=student/2;
+    std::cout << student << std::endl;
+
+    return 0;
+
+    return 0;
+}
