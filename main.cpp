0807f4e088b04d43f02ee96211c89045e7b578cf
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-21 12:16:44 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iomanip>
+#include <cmath>
+
+#include <iostream>
+#include <valarray>
+
+//Math function :
+
+
+int main() {
+    double x=3.9;
+    double y=4;
+    double z=0;
+
+    z= std::max(x,y);
+    z= std::min(x,y);
+    z= std::pow(y,z);
+    z= std::sqrt(10);
+    z=std::abs(y);
+
+
+    return 0;
+}
