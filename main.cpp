d7b8c34e144cd634dd0bfb81ef786adc4517c6fb
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-21 12:06:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iomanip>
+#include <iostream>
+
+//User Input :
+// cout << insertion operator
+// cin << extraction operator
+
+int main() {
+    int age;
+    std::string Name;
+
+    std::cout <<"What is your full name: ";
+    std::getline(std::cin >>std::ws, Name);
+
+    // Input for age
+    std::cout <<"What is your age: ";
+    std::cin >> age;
+
+    // Output results
+    std::cout <<"Your age is " << age << " years old." << std::endl;
+
+    return 0;
+}
