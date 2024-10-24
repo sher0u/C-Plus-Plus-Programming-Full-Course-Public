21bb12eaed6811d9566fe8c27991ae2b579df1ce
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-24 13:24:34 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
 #include <ctime>
 
-//Small Project Random event generator
+//Small Project Number Guessing Game
 
 
 int main() {
-    srand(time(0));
-    int randomNumber = rand() % 5 +1;
 
-    switch (randomNumber) {
-        case 1:std::cout<<"win 1 dollar";
-        break;
-        case 2:std::cout<<"win 2 dollar";
-        break;
-        case 3:std::cout<<"win 5 dollar";
-        break;
-        case 4:std::cout<<"win 6 dollar";
-        break;
-        case 5:std::cout<<"win 10 dollar";
-        break;
+    int Number;
+    int tries;
+    int guess;
+
+    std::srand(time(NULL)); // Initialize random seed
+
+    Number=rand()%10+1;
+    tries=0;
+
+    std::cout << " welcome to number guessing game 1 try = 1 dollar " << std::endl;
+    std::cout << "Enter How many tries you wanna play:"  << std::endl;
+    std::cin >> tries;
+
+    std::cout << "You payed for " <<tries << " tries" << std::endl;
+    std::cout <<"Enter you guess from 0 to 10"<< std::endl;
+    std::cin >> guess;
+
+    for (int i = 0; i < tries; i++) {
+        std::cin >> guess;
+
+        if (guess == Number) {
+            std::cout << "Congratulations! You guessed the correct number: " << guess << std::endl;
+            break; // End the game if the player guesses correctly
+        } else {
+            std::cout << "Wrong guess. Try again." << std::endl;
+        }
     }
+    std::cout << "Game over thanks for playing"<< std::endl;
+
     return 0;
 }
