428b58be48b710b13c0d112393a6c29bbd608709
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-24 12:54:57 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <ctime>
 
-//Random number generator
+//Small Project Random event generator
 
 
 int main() {
+    srand(time(0));
+    int randomNumber = rand() % 5 +1;
 
-    srand(time(NULL));
-
-    int Num=rand()%6; // what after %is the range ;
-    std::cout << Num << std::endl;
+    switch (randomNumber) {
+        case 1:std::cout<<"win 1 dollar";
+        break;
+        case 2:std::cout<<"win 2 dollar";
+        break;
+        case 3:std::cout<<"win 5 dollar";
+        break;
+        case 4:std::cout<<"win 6 dollar";
+        break;
+        case 5:std::cout<<"win 10 dollar";
+        break;
+    }
     return 0;
 }
