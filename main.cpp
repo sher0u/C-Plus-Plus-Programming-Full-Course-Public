b4186d6e808881f21c0f8183dcf2f6aaabee8c6e
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-26 02:07:01 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 
-// For each Loop = loop that eases the traversal over an iterable data set
+// Pass Array to fucntion;
+double GetTheSomme(double prices[],int size) {
+    double sum=0;
+    for(int i=0;i<size;i++) {
+        sum+=prices[i];
+    }
+    return sum;
+}
 
 int main() {
-    int Numbers[5]={1,2,3,4,5};
-    for (int i : Numbers) {
-        std::cout << i << std::endl;
-    }
+
+    double price[5]={43.00,11.00,75.00,342.00,12.00};
+    int razmer=0;
+    double somme=0;
+
+    razmer=sizeof(price)/sizeof(price[0]);
+
+    somme=GetTheSomme(price,razmer);
+    std::cout<<somme;
 
     return 0;
 }
+
