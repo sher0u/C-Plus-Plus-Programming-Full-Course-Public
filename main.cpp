cac3720c86e9d3bf0c462d4f334e713ffb28774e
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-26 02:30:49 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 
-// Pass Array to fucntion;
-double GetTheSomme(double prices[],int size) {
-    double sum=0;
-    for(int i=0;i<size;i++) {
-        sum+=prices[i];
-    }
-    return sum;
-}
+// Search an element in array
 
 int main() {
+    int Numbers[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(Numbers) / sizeof(Numbers[0]);
+    int number;
+    bool found = false; // To track if the number was found
 
-    double price[5]={43.00,11.00,75.00,342.00,12.00};
-    int razmer=0;
-    double somme=0;
+    std::cout << "Enter the number you're searching for from 1 to 10: " << std::endl;
+    std::cin >> number;
 
-    razmer=sizeof(price)/sizeof(price[0]);
+    for (int i = 0; i < size; i++) {
+        if (Numbers[i] == number) {
+            std::cout << "Number found: " << Numbers[i] << std::endl;
+            found = true; // Set found to true
+            break; // Exit the loop since we found the number
+        }
+    }
 
-    somme=GetTheSomme(price,razmer);
-    std::cout<<somme;
+    if (!found) {
+        std::cout << "Number not found." << std::endl; // Only print if not found
+    }
 
     return 0;
 }
-
