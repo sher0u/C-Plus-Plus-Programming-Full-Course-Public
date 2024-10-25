e8f1d1eff73b54bfb01f12d52b5b27392c99d3d8
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2024-10-26 00:34:09 +0100

    Update and rename main.cpp to A28 Pass Array to fucntion.cpp

---
+++ A28 Pass Array to fucntion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+// Pass Array to fucntion;
+double GetTheSomme(double prices[],int size) {
+    double sum=0;
+    for(int i=0;i<size;i++) {
+        sum+=prices[i];
+    }
+    return sum;
+}
+
+int main() {
+
+    double price[5]={43.00,11.00,75.00,342.00,12.00};
+    int razmer=0;
+    double somme=0;
+
+    razmer=sizeof(price)/sizeof(price[0]);
+
+    somme=GetTheSomme(price,razmer);
+    std::cout<<somme;
+
+    return 0;
+}
